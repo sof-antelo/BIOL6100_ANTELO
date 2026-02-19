be31d20f2db16a97184333cf635c3b63fa73ec1f
Jane Doe <jane@example.com>
2026-02-19 14:18:07 -0500

    class 2/19

--- r_scripts/for_loops.R
+++ r_scripts/for_loops.R
@@ -180,3 +180,174 @@ head(growthDF)
 
 plot(growthDF$r, y = growthDF$N) #kind of like a summary of the model rather than just one run
 ##############
+
+# 1. Initialize vector of 1000 r values from 0 to 1
+r_values <- seq(0, 1, length.out = 1000)
+
+# 2. Initialize storage container
+max_n_values <- numeric(length(r_values))
+
+# 3. Loop through r values
+for (i in seq_along(r_values)) {
+  temp <- logistic_growth(r = r_values[i])
+  max_n_values[i] <- max(temp$population)
+}
+
+# make data frame
+df <- data.frame(r_values, max_n_values)
+names(df) <- c("r", "max(N)")
+
+
+plot(x = df$r, y = df$`max(N)`)
+
+
+
+
+
+##################################
+# 2D parameter sweep for log growth function
+# outputs matrix
+
+
+# vectors for r and k parameters
+r_values <- seq(0, 1, length.out = 100)
+K_values <- seq(10, 1000, length.out = 100)
+
+
+growth_sweep <- function(rvec, kvec){
+
+  # create storage matrix for outputs
+  stor_mat <- matrix(NA, nrow = length(rvec), ncol = length(kvec))
+
+  for (i in seq_along(rvec)){ # rows
+    for (j in seq_along(kvec)){ # cols
+
+      # run log growth
+      tmp_df <- logistic_growth(r = rvec[i], K = kvec[j])
+
+      # store max n in 2d matrix
+      stor_mat[i,j] <- max(tmp_df$population)
+    }
+  }
+  return(stor_mat)
+}
+
+# run growth param sweep
+growth_mat <- growth_sweep(rvec = r_values, kvec = K_values)
+
+
+
+
+
+
+
+
+
+##################################
+# 2D parameter sweep for log growth function
+# outputs a data frame
+
+numParms <- 100
+
+
+# vectors for r and k parameters
+r_values <- seq(0, 1, length.out = numParms)
+K_values <- seq(10, 1000, length.out = numParms)
+
+
+
+growth_sweep <- function(rvec, kvec){
+
+  # row indexing variable
+  counter <- 1
+
+  # create storage dataframe
+  dfLength <- length(rvec)*length(kvec) # how long is DF
+  r_out <- rep(NA, dfLength)
+  k_out <- rep(NA, dfLength)
+  maxn_out <- rep(NA, dfLength)
+
+  # turn vecs into df
+  storageDF <- data.frame(r_out, k_out, maxn_out)
+
+
+  # enter our loops
+  for (i in seq_along(rvec)){ # rows
+    for (j in seq_along(kvec)){ # cols
+
+      # run log growth
+      tmp_df <- logistic_growth(r = rvec[i], K = kvec[j])
+
+      # store max n in dataframe
+      storageDF$maxn_out[counter] <- mean(tmp_df$population)
+      storageDF$r_out[counter] <- rvec[i] # value for r stored
+      storageDF$k_out[counter] <- kvec[j] # value for k stored
+
+      counter <- counter + 1 # increase counter
+    }
+  }
+  return(storageDF)
+}
+
+# run growth param sweep
+df <- growth_sweep(rvec = r_values, kvec = K_values)
+
+df
+
+ggplot(data = df, aes(x = r_out, y = k_out, fill= maxn_out)) + 
+  geom_tile()
+
+
+
+
+
+
+
+
+
+# create a random walk function
+
+###############################
+# NAME: ran_walk
+# purpose: conducts a random walk
+# input:times = number of time steps
+#       n1 = initial pop size
+#       lamda = finite rate of increase
+#.      noise_sd = 10
+# output:
+#          vector n with population size > 0 until extinct.
+#
+library(ggplot2)
+
+
+
+ran_walk <- function(times = 100, n1=50, lamda = 1, noise_sd = 10){
+  n <- rep(NA, times) # create our output vec
+  n[1] <- n1 # initialize init pop size
+  noise <- rnorm(n = 100, mean = 0, sd = noise_sd) # created noise/error
+
+  for (i in 1:(times-1)){
+    n[i + 1] <- lamda*n[i] + noise[i]
+    if(n[i + 1] <= 0){
+      n[i + 1] <- NA
+      cat("Population extinction at time", i+1, "\n")
+      break
+    }
+  }
+  return(n)
+}
+
+x <- ran_walk()
+print(x)
+
+# plotting with default values
+qplot(x=1:100, y=ran_walk(), geom="line")
+
+# no noise/not so random walk
+qplot(x=1:100, y=ran_walk(noise_sd=0), geom="line")
+
+# no noise and adjust lamda
+qplot(x=1:100, y=ran_walk(lamda = 0.92, noise_sd=0), geom="line")
+
+# add some stoch. make lamda > 1
+qplot(x=1:100, y=ran_walk(lamda = 1.01, noise_sd=6), geom="line")
---
+++ r_scripts/ggplot.R
@@ -0,0 +1,133 @@
+# learning the basics of ggplot
+# Sof Antelo
+# 2/19/26
+
+################################
+
+# p1 <- ggplot(data = <DATA>) +
+#   aes(<MAPPINGS>) +
+#   <GEOM_FUNCTION>(aes(<MAPPINGS>),
+#                   stat=<STAT>,
+#                   position=<POSITION>) +
+#                   <COORDINATE_FUNCTION> +
+#                   <FACET_FUNCTION> #makes multiple plots all together
+
+library(ggplot2)
+library(ggthemes)
+library(patchwork)
+
+#load dataset
+d <- mpg
+
+###first call to ggplot: histogram
+ggplot(data = d) + 
+    aes(x = hwy) +
+    geom_histogram()
+
+##change color of histogram
+#########cornflowerblue is the best r color
+ggplot(data = d) + 
+    aes(x = hwy) +
+    geom_histogram( fill = "cornflowerblue", color ="black" )
+
+### Density plot
+ggplot(data = d) +
+  aes(x = hwy) +
+  geom_density(fill = "cornflowerblue")
+
+### scatter plot
+ggplot(data = d) +
+  aes(x = displ, y = hwy) +
+  geom_point() +
+  geom_smooth() +
+  geom_smooth() +
+  geom_smooth(method = "lm", col = "lightpink2")
+
+
+###boxplot
+ggplot(data = d) +
+  aes(x = fl, y = cty) +
+  geom_boxplot(fill = "paleturquoise1", color = "cornflowerblue")
+
+### barplot
+ggplot(data = d) +
+  aes(x = fl) +
+  geom_bar()
+
+#barplot with y reponse
+x_treatment <- c("control", "low", "high")
+y_response <- c(12, 2.5, 22)
+summary_data <- data.frame(x_treatment, y_response)
+
+ggplot(data = summary_data) +
+  aes(x = x_treatment, y = y_response) +
+  geom_col(fill = c("seashell4","cornflowerblue", "plum3"),)
+
+#plotting curves 
+ # basic curves and functions
+ my_vec <- seq(1,100,by=0.1)
+ 
+ # plot simple mathematical functions
+ d_frame <- data.frame(x=my_vec,y=sin(my_vec))
+ ggplot(data=d_frame) +
+   aes(x=x,y=y) +
+   geom_line()
+
+ # plot probability functions
+ d_frame <- data.frame(x=my_vec,y=dgamma(my_vec,shape=5, scale=3))
+ ggplot(data=d_frame) +
+   aes(x=x,y=y) +
+   geom_line()
+
+#themes and fonts
+p2 <- ggplot(data = d, mapping = aes(x=displ, y = cty))+
+  geom_point()
+p2
+
+p2 + theme_classic()
+#if there's a lot of things to add, its best to just add it to the object
+p2 + theme_minimal() 
+p2 + theme_linedraw()
+p2 + theme_base() #base r graphics
+p2 + theme_par() #takes parameters and uses those
+
+#changing font and font size
+p2 + theme_classic(base_size = 25, base_family = "serif")
+
+#adding additional fonts
+library(extrafont)
+font_import() #imports all system fonts
+
+p3 + theme_classic(base_size=25,
+                  base_family = "")
+
+p3 <- ggplot(data=d, mapping=aes(x=fl, fill=fl))+
+      geom_bar()
+p3
+#uses default colors and includes a key
+p3 + coord_flip() + theme_grey(base_size=20,base_family="sans")
+
+ # use x and/or y limits to clip data set
+
+ p2 <- ggplot(data=d) +
+  aes(x=displ,y=cty) + 
+  geom_point() +
+ labs(title="My graph title here",
+      subtitle="An extended subtitle",
+      x="Displacement",
+      y="City Mileage",
+      caption="Add a caption here") +
+   theme_bw(base_size=25,base_family="Monaco")
+  #    xlim(0,4) + ylim(0,20)
+ print(p2)
+
+
+ p2 <- ggplot(data=d) +
+  aes(x=displ,y=cty) + 
+ geom_point(size=4,
+            shape=21,
+            color="black",fill="cyan") +
+   xlim(4,7) + 
+   ylim(-10,40) +
+   theme_bw(base_size=25,base_family="Monaco")
+ print(p2)
